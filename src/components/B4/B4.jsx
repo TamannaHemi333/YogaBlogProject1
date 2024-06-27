a2023b031d73deb2f09e2ab8681fcf2d550c7c7f
TamannaHemi333 <141430359+TamannaHemi333@users.noreply.github.com>
2024-06-28 00:02:27 +0600

    Add files via upload

---
+++ src/components/B4/B4.jsx
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+import './B4.css';
+
+const B4 = () => {
+  return (
+    <Fragment>
+      <div className="container45">
+        <div className="section">
+          <h1><u>Archives</u></h1>
+          <p>April 2021</p>
+        </div>
+
+        <div className="section">
+          <h1><u>Categories</u></h1>
+          <p>1. creativity</p>
+          <p>2. Fitness</p>
+          <p>3. Health</p>
+          <p>4. Meditation</p>
+          <p>5. yoga</p>
+        </div>
+
+        <div className="section">
+          <h1><u>Meta</u></h1>
+          <p>1. Register</p>
+          <p>2. Log in</p>
+          <p>3. Entries feed</p>
+          <p>4. Comments feed</p>
+          <p>5. WordPress.org</p>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default B4;
